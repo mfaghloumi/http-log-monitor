436b9654f907134e4b9f84d30f40b0ff326d4921
Monssef <mfaghloumi@gmail.com>
2019-03-31 18:32:56 +0200

    Add command line parser

--- src/main/java/com/datadog/logmonitor/Main.java
+++ src/main/java/com/datadog/logmonitor/Main.java
@@ -6,6 +6,8 @@ import com.datadog.logmonitor.pubsub.publisher.HitPublisher;
 import com.datadog.logmonitor.pubsub.publisher.ScheduledPublisher;
 import com.datadog.logmonitor.pubsub.subscriber.HitsPerSecondSubscriber;
 import com.datadog.logmonitor.pubsub.subscriber.HitsPerSectionSubscriber;
+import com.datadog.logmonitor.utils.CommandUtils;
+import com.datadog.logmonitor.utils.CommandUtils.Parameters;
 import org.apache.commons.io.input.Tailer;
 
 import java.io.File;
@@ -17,14 +19,11 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 //TODO Builder for P/S Framework
 public class Main {
 
-    private static final String DEFAULT_ACCESS_LOG = "access.log";
-
     private static final int DELAY_MILLIS = 500;
 
-    private static final double DEFAULT_THRESHOLD = 10;
-
-    //TODO Find a better way for the publisher
     public static void main(String[] args) {
+
+        Parameters parameters = CommandUtils.parse(args);
 
         PubSub pubsub = new PubSub();
 
@@ -33,12 +32,13 @@ public class Main {
         ScheduledPublisher aggregatePerSecondPublisher = new ScheduledPublisher(pubsub,
                 Messages::aggregatePerSecondMessage, 0, 1, SECONDS);
         HitPublisher hitPublisher = new HitPublisher(pubsub);
-        Tailer tailer = new Tailer(new File(DEFAULT_ACCESS_LOG), hitPublisher, DELAY_MILLIS, true);
+        Tailer tailer = new Tailer(new File(parameters.getFilename()), hitPublisher, DELAY_MILLIS, true);
 
-        pubsub.subscribe(new HitsPerSecondSubscriber(DEFAULT_THRESHOLD), NEW_HIT, AGGREGATE_PER_SECOND);
+        pubsub.subscribe(new HitsPerSecondSubscriber(parameters.getThreshold()), NEW_HIT, AGGREGATE_PER_SECOND);
         pubsub.subscribe(new HitsPerSectionSubscriber(), NEW_HIT, AGGREGATE_PER_SECTION);
 
         tailer.run();
+
     }
 
 
---
+++ src/main/java/com/datadog/logmonitor/utils/CommandUtils.java
@@ -0,0 +1,64 @@
+package com.datadog.logmonitor.utils;
+
+import lombok.Data;
+import lombok.experimental.UtilityClass;
+import org.apache.commons.cli.*;
+
+import java.util.Optional;
+
+@UtilityClass
+public class CommandUtils {
+
+    private final String CMD_NAME = "http-log-monitor";
+
+    private static final String DEFAULT_ACCESS_LOG = "access.log";
+
+    private static final String DEFAULT_THRESHOLD = "10";
+
+    public Parameters parse(String... args) {
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        Options options = new Options();
+        Option fileOption = Option.builder("f")
+                .longOpt("file")
+                .hasArg()
+                .desc("HTTP log file path, default value is : " + DEFAULT_ACCESS_LOG)
+                .type(String.class)
+                .build();
+        Option thresholdOption = Option.builder("t")
+                .longOpt("threshold")
+                .hasArg()
+                .desc("Alerting threshold average hits/second, default value is : " + DEFAULT_THRESHOLD)
+                .type(Double.class)
+                .build();
+        options.addOption(fileOption);
+        options.addOption(thresholdOption);
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException pe) {
+            formatter.printHelp(CMD_NAME, options);
+            System.exit(-1);
+        }
+        String filename = cmd.getOptionValue("file", DEFAULT_ACCESS_LOG);
+        Double threshold = null;
+        try {
+            threshold = Optional.of(cmd.getOptionValue("threshold", DEFAULT_THRESHOLD))
+                    .map(Double::parseDouble)
+                    .get();
+        } catch (NumberFormatException e) {
+            formatter.printHelp(CMD_NAME, options);
+            System.exit(-1);
+        }
+        return new Parameters(filename, threshold);
+    }
+
+    @Data
+    public static class Parameters {
+
+        private final String filename;
+
+        private final Double threshold;
+
+    }
+}
